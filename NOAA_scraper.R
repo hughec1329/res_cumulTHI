7c4e8a15f72b1f902fcdf2d172f62aed72f29b46
hughec1329 <hughcrockford@gmail.com>
2013-04-02 22:00:33 -0700

    added NOAA scraper - but does not allow humidity data

---
+++ NOAA_scraper.R
@@ -0,0 +1,36 @@
+# cumul_THI project
+# function to scrape noaa site
+# 20120322 HCrockford
+
+library(RCurl)
+library(RHTMLForms)
+library(rjson)
+token = "nnHAnvvghOAhHpmKEZtTWWplMXQCQvto"
+
+
+vmtrc = c(lat = 36.132156,long = -119.321694)
+data = getForm("http://www.ncdc.noaa.gov/cdo-services/services/datasets",token = token, .opts = list(httpheader = c("Accept" = "application/xml"))) #  get available datasets in XML
+data = getForm("http://www.ncdc.noaa.gov/cdo-services/services/datasets",token = token) #  get available datasets in JSON
+fromJSON(data)
+
+loc = getForm("http://www.ncdc.noaa.gov/cdo-web/webservices/cdows_locationsearch", token = token)
+
+p = getForm("http://www.ncdc.noaa.gov/cdo-services/services/datasets/GHCND/locations/ZIP:93274/datatypes",token = token)
+p = getForm("http://www.ncdc.noaa.gov/cdo-services/services/datasets/GHCND/locations?latitude=36.132156&longitude=-119.321694/datatypes",token = token)
+p = getForm("http://www.ncdc.noaa.gov/cdo-services/services/datasets/GHCND",token = token)
+
+o =  getForm("http://www.ncdc.noaa.gov/cdo-services/services/datasets/GHCND/locationsearch?latitude=36.132156&longitude=-119.321694",token = token, radius = 25)	# get stations 25 clicks around vmtrc
+t = unlist(strsplit(o,'"'))
+stats = t[grepl("GHCND",t)]	# stations
+
+       q = getForm(sprintf("http://www.ncdc.noaa.gov/cdo-services/services/datasets/GHCND/stations/%s/datatypes",i), token = token) # tulare doesnt have temp, visalia does
+
+w = sapply(stats, function(i){
+       Sys.sleep(1)
+       getForm(sprintf("http://www.ncdc.noaa.gov/cdo-services/services/datasets/GHCND/stations/%s/datatypes",i), token = token)
+})
+
+grep("Dew",w)                          #  none of the datasets accesible by CDO/REST have Humid !!!
+
+
+
